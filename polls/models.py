6552522516c8367483ce8c6d0afbda5063536adc
Ty Ty <tyty@abc.com>
2022-05-08 14:10:47 +0700

    update

--- polls/models.py
+++ polls/models.py
@@ -5,9 +5,24 @@ from django.db import models
 
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+    YEAR_IN_SCHOOL_CHOICES = [
+        ('FR', 'Freshman'),
+        ('SO', 'Sophomore'),
+        ('JR', 'Junior'),
+        ('SR', 'Senior'),
+        ('GR', 'Graduate'),
+    ]
+
+    question_text = models.CharField(max_length=200,)
     pub_date = models.DateTimeField('date published')
     rating = models.IntegerField(default=0)
+    question_display = models.TextField(max_length=255, null=True)
+    active = models.BooleanField(default=False)
+    year_in_school = models.CharField(
+        max_length=2,
+        choices=YEAR_IN_SCHOOL_CHOICES,
+        default='FR',
+    )
 
     def __str__(self):
         return self.question_text
--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,32 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Question
+
+import json
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    data = {
+        'name': 'Raghav',
+        'location': 'India',
+        'is_active': False,
+        'count': 28,
+        'list': ['question 1', 'question 2', 'question 3']
+    }
+    dump = json.dumps(data)
+    return HttpResponse(dump)
+
+
+def renderPageViewHtml(request):
+    list_question = Question.objects.all()
+    choice_question = Question.YEAR_IN_SCHOOL_CHOICES
+    print(choice_question)
+    context = {
+        'name': 'Nguyễn Văn Tý',
+        'title': 'Developer',
+        'is_active': False,
+        'list_question': list_question,
+        'choice_question': choice_question,
+    }
+    return render(request, 'polls/index.html', context)
